96be6f639288477f4769a911f6e2f8f3f7f8fd12
Kacper Uminski <kacperum@gmail.com>
2020-04-19 14:44:10 +0200

    Added icons to Xmobar.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -16,6 +16,7 @@ import XMonad.Hooks.SetWMName
 
 -- Layouts
 import XMonad.Layout.Gaps
+import XMonad.Layout.Renamed (renamed, Rename(CutWordsLeft, Replace))
 import XMonad.Layout.Spacing
 
 -- Utilities
@@ -44,15 +45,14 @@ main = do
     xmonad $ docks defaultConfig
         { manageHook = insertPosition End Newer <+> manageDocks <+> manageHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = \x -> hPutStrLn xmproc x
-                        , ppCurrent = xmobarColor "#c3e88d" "" . wrap "[" "]" -- Current workspace in xmobar
-                        , ppVisible = xmobarColor "#c3e88d" ""                -- Visible but not current workspace
-                        , ppHidden = xmobarColor "#82AAFF" "" . wrap "*" ""   -- Hidden workspaces in xmobar
-                        , ppHiddenNoWindows = xmobarColor "#F07178" ""        -- Hidden workspaces (no windows)
-                        , ppTitle = xmobarColor "#d0d0d0" "" . shorten 80     -- Title of active window in xmobar
-                        , ppSep =  "<fc=#9AEDFE> : </fc>"                     -- Separators in xmobar
-                        , ppUrgent = xmobarColor "#C45500" "" . wrap "!" "!"  -- Urgent workspace
-                        , ppOrder  = \(ws:l:t:ex) -> [ws,l]++ex
+                        { ppOutput =          \x -> hPutStrLn xmproc x
+                        , ppCurrent =         xmobarColor "#63f2f1" "" . wrap "[" "]"    -- Current workspace in xmobar
+                        , ppVisible =         xmobarColor "#65b2ff" ""                   -- Visible but not current workspace
+                        , ppHidden =          xmobarColor "#65b2ff" "" . wrap "*" ""     -- Hidden workspaces in xmobar
+                        , ppHiddenNoWindows = xmobarColor "#906cff" ""                   -- Hidden workspaces (no windows)
+                        , ppSep =             " : "                                      -- Separators in xmobar
+                        , ppUrgent =          xmobarColor "#ff5458" "" . wrap "!" "!"    -- Urgent workspace
+                        , ppOrder  =          \(ws:l:t:ex) -> [ws,l]++ex
                         }
         , normalBorderColor  = "#1b182c"
         , focusedBorderColor = "#906cff"
@@ -86,10 +86,10 @@ myKeys =
 ------------------------------------------------------------------------
 myLayoutHook = avoidStruts $ myDefaultLayout
 
-             where
-                 myDefaultLayout = tall
+    where
+        myDefaultLayout = tall
 
-tall = spacing 20 $ gaps [(U,20), (D,20), (L,20), (R,20)] $ Tall 1 (3/100) (1/2)
+tall = renamed [Replace "tall"] $ spacing 20 $ gaps [(U,20), (D,20), (L,20), (R,20)] $ Tall 1 (3/100) (1/2)
 
 
 ------------------------------------------------------------------------
