770b8cc91a1590734b677f6b4dae6c9ebdd4da61
kacper-uminski <kacperum@gmail.com>
2023-11-30 13:37:38 +0100

    Changing to Hyprland.

--- .config/nixos/main.nix
+++ .config/nixos/main.nix
@@ -64,6 +64,76 @@
 
       programs = {
 
+        alacritty = {
+          enable = true;
+          settings = {
+            colors = {
+              primary = {
+                background = "#000000";
+                foreground = "#d8dee9";
+                dim_foreground = "#a5abb6";
+              };
+              cursor = {
+                text = "#2e3440";
+                background = "#d8dee9";
+              };
+              selection = {
+                text = "#000000";
+                background = "#d8dee9";
+              };
+              search = {
+                matches = {
+                  text = "#000000";
+                  background = "#d8dee9";
+                };
+                footer_bar = {
+                  foreground = "#d8dee9";
+                  background = "#434c5e";
+                };
+              };
+              normal = {
+                black = "#000000";
+                red = "#bf616a";
+                green = "#a3be8c";
+                yellow = "#ebcb8b";
+                blue = "#81a1c1";
+                magenta = "#b48ead";
+                cyan = "#88c0d0";
+                white = "#e5e9f0";
+              };
+              bright = {
+                black = "#4c566a";
+                red = "#bf616a";
+                green = "#a3be8c";
+                yellow = "#ebcb8b";
+                blue = "#81a1c1";
+                magenta = "#b48ead";
+                cyan = "#88c0d0";
+                white = "#e5e9f0";
+              };
+              dim = {
+                black = "#000000";
+                red = "#94545d";
+                green = "#809575";
+                yellow = "#b29e75";
+                blue = "#68809a";
+                magenta = "#8c738c";
+                cyan = "#6d96a5";
+                white = "#aeb3bb";
+              };
+            };
+            font = {
+              normal.family = "IBM Plex Mono";
+              glyphs.family = "Font Awesome";
+              size = 12;
+            };
+            window.padding = {
+              x = 20;
+              y = 20;
+            };
+          };
+        };
+
         bash = {
           enable = true;
           enableCompletion = true;
@@ -312,10 +382,6 @@
       "/share/zsh"
     ];
 
-#    variables = {
-#      QT_STYLE_OVERRIDE="kvantum";
-#    };
-
     # List packages installed in system profile.
     systemPackages = with pkgs; [
       (let
@@ -377,7 +443,7 @@
       unzip
       usbutils
       uutils-coreutils
-      valgrind
+      valgrind # Memory profiler for C/C++
       vifm
       wget
       wiki-tui
