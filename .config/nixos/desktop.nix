0a0b52c1c74f9adc9b082373287f5a498c9eaeb0
kacper-uminski <kacperum@gmail.com>
2023-12-07 22:10:28 +0100

    Reinstalled onto 23.11

--- .config/nixos/desktop.nix
+++ .config/nixos/desktop.nix
@@ -60,6 +60,9 @@
 
   home-manager = {
     users.kacper = {pkgs, ...}: {
+	    home = {
+        stateVersion = "23.11";
+	    };
       programs = {
         alacritty = {
           settings = {
@@ -171,4 +174,12 @@
     dconf.enable = true;
   };
 
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
